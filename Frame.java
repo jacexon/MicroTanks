1b1f812ff1867e4cb5fde62ff1f6617a6b698411
Jacek <polakjacek@gmail.com>
2016-04-10 10:50:39 +0200

    Update Frame.java

--- Frame.java
+++ Frame.java
@@ -2,10 +2,15 @@ import javax.swing.*;
 import java.awt.*;
 
 /**
- * Created by Bartłomiej on 2016-03-26.
+ * Klasa <code>Frame</code> jest abstrakcyjna i dostarcza szereg danych i metod klasom pochodnym.
+ * @author      Bartłomiej Bielecki <bartlomiejbielecki1@gmail.com>
+ * @author      Jacek Polak <polakjacek@gmail.com>
+ * @version     1.1
+ * @since       2016-03-26
  */
-public abstract class Frame {
+public abstract class Frame extends JFrame{
 
+    //region Dane ekranowe
     Toolkit kit = Toolkit.getDefaultToolkit();
     Dimension dim = kit.getScreenSize();
     protected int width = (int) dim.getWidth();
@@ -13,19 +18,37 @@ public abstract class Frame {
     protected int buttonHeight = height/8;
     protected int buttonWidth = width/4;
     Font ft2 = new Font("Comic", Font.BOLD, 25);
+    //endregion
 
+    /**
+     * Metoda tworząca ramkę w zależności od podanych parametrów.
+     * @param width Szerokość ramki
+     * @param height Wysokość ramki
+     * @param name Tytuł ramki
+     * @return Zbudowana ramka
+     */
     protected JFrame createAFrame(int width, int height, String name){
 
         JFrame frame = new JFrame(name);
+
 
         frame.setSize(width, height);
         frame.setResizable(true);
         frame.setLocation(150, 150);
         frame.setLayout(null);
+        frame.setBackground( new Color(21,185,125));
 
         return frame;
     }
 
+    /**
+     * Metoda tworząca przycisk w zależności od podanych parametrów.
+     * @param width Szerokość przycisku
+     * @param height Wysokość przycisku
+     * @param numberOfButton Numer przycisku
+     * @param name Napis na przycisku
+     * @return Utworzony przycisk
+     */
     protected JButton createAButton(int width, int height, int numberOfButton, String name) {
         int buttonHeight = height/8;
         int buttonWidth = width/4;
@@ -40,7 +63,15 @@ public abstract class Frame {
 
     }
 
+    /**
+     * Metoda zwracająca szerokość ekranu.
+     * @return Szerokość ekranu
+     */
     public int getWidth(){return width;}
-    public int getHeight(){return height;}
 
+    /**
+     * Metoda zwracająca wysokość ekranu.
+     * @return Wysokość ekranu
+     */
+    public int getHeight(){return height;}
 }
