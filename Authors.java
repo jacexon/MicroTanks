abd76005ac96842519e5d3b2c254a2eff903932a
Jacek <polakjacek@gmail.com>
2016-04-10 10:48:15 +0200

    Update Authors.java

--- Authors.java
+++ Authors.java
@@ -2,20 +2,47 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 /**
- * Created by admin on 2016-03-27.
+ * Klasa <code>Authors</code> zawiera informacje o
+ * autorach gry, betatesterach i ludziach, którym
+ * należą się podziękowania za cierpliwość.
+ * Klasa dziedziczy po klasie abstrakcyjnej <code>Frame</code>
+ *
+ * @author      Bartłomiej Bielecki <bartlomiejbielecki1@gmail.com>
+ * @author      Jacek Polak <polakjacek@gmail.com>
+ * @version     1.1
+ * @since       2016-03-26
  */
+
 public class Authors extends Frame
 {
-    Font title = new Font("Sans Serif", Font.BOLD, 15);
+    private Font title = new Font("Sans Serif", Font.BOLD, 15);
 
+    /**
+     * Konstruktor klasy <code>Authors</code>, który inicjalizuje
+     * wielkość ramki w zależności od rozmiaru ekranu urządzenia wykonującego
+     * program.
+     */
     public Authors()
     {
         this.width=width/2;
         this.height=height*3/4;
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent we) {
+                dispose();
+            }
+        });
     }
 
+    /**
+     * Metoda ta odpowiedzialna jest za utworzenie ramki <code>Authors</code>.
+     * @param authorsFrame Ramka, w której tworzone są niezbędne komponenty do przechowywania pól tekstowych
+     * @param mainMenuF Ramka menu głównego, do którego powracamy wychodząc z zakładki <code>Authors</code>
+     * @return Brak
+     */
     public void createAnAuthorsFrame(JFrame authorsFrame, JFrame mainMenuF)
     {
         JButton backButton = createAButton(width, height, 5, "BACK");
