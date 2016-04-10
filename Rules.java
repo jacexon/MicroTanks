f2629219f5f9e0b45192ff9cf78ed29cab58b7cc
Jacek <polakjacek@gmail.com>
2016-04-10 10:54:25 +0200

    Update Rules.java

--- Rules.java
+++ Rules.java
@@ -2,19 +2,45 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 /**
- * Created by admin on 2016-03-27.
+ * Klasa <code>Rules</code> odpowiada za objaśnienie zasad gry w MicroTanks.
+ * Dziedziczy ona po klasie abstrakcyjnej <code>Frame</code>
+ *
+ * @author      Bartłomiej Bielecki <address @ example.com>
+ * @author      Jacek Polak <polakjacek@gmail.com>
+ * @version     1.1
+ * @since       2016-03-26
  */
 public class Rules extends Frame
 {
-    Font title = new Font("Sans Serif", Font.BOLD, 20);
+    private Font title = new Font("Sans Serif", Font.BOLD, 20);
+
+    /**
+     * * Konstruktor klasy <code>Rules</code> inicjalizuje  ramki w zależności od
+     * rozmiaru ekranu urządzenia wykonującego
+     * program.
+     */
     public Rules()
     {
         this.width=width/2;
         this.height=height*3/4;
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent we) {
+                dispose();
+            }
+        });
     }
 
+    /**
+     * Metoda tworząca ramkę z polami tekstowymi, w tym jedno zawierające
+     * zasady.
+     * @param rulesFrame Ramka zawierająca zasady
+     * @param mainMenuF Ramka menu głównego, do którego można powrócić za pomocą przycisku
+     * @return Brak
+     */
     public void createARulesFrame (JFrame rulesFrame, JFrame mainMenuF)
     {
         JButton backButton = createAButton(width, height, 5, "BACK");
@@ -42,7 +68,7 @@ public class Rules extends Frame
             public void actionPerformed(ActionEvent e) {
                 rulesFrame.setVisible(false);
                 mainMenuF.setVisible(true);
-                }
+            }
         });
 
     }
