178b6a739ebed1f2ee8439f12a08af4ae7c5caeb
Jacek <polakjacek@gmail.com>
2016-04-10 10:52:44 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,34 @@
 import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
 
 /**
- * Created by Bartłomiej and Jacek on 2016-03-26.
+ * <h1>Witaj w MicroTanks!</h1>
+ *  MicroTanks to gra zręcznościowa, która od chwili pierwszego
+ *  uruchomienia będzie towarzyszyła Ci przez długi czas!
+ *
+ * @author      Bartłomiej Bielecki <address @ example.com>
+ * @author      Jacek Polak <polakjacek@gmail.com>
+ * @version     1.1
+ * @since       2016-03-26
  */
+
 public class Main {
-    public static void main(String[] args) {
+    /**
+     * Funkcja main inicjalizuje rozgrywkę.
+     * @param args Nieużywane
+     * @throws IOException
+     * @return Brak
+     */
+    public static void main(String[] args) throws IOException {
 
         MainMenu mainMenu = new MainMenu();
-        JFrame mainMenuFrame= mainMenu.createAFrame(mainMenu.getWidth(),mainMenu.getHeight(),"Micro Tanks");
+        JFrame mainMenuFrame= mainMenu.createAFrame(mainMenu.getWidth(),mainMenu.getHeight(),"Main Menu");
+        mainMenuFrame.setForeground(new Color(21,185,125));
         mainMenu.createFrameButtons(mainMenuFrame);
         mainMenuFrame.setVisible(true);
+
+
+
     }
 }
