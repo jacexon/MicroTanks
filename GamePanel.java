fd0d0c437b9e169364cf31d827c17f13810e4a17
Jacek <polakjacek@gmail.com>
2016-05-21 15:01:12 +0200

    Update GamePanel.java

--- GamePanel.java
+++ GamePanel.java
@@ -13,14 +13,18 @@ import java.util.Properties;
  */
 public class GamePanel extends JPanel implements KeyListener {
     private int width,height;
-    private int numberOfTanks=3;
+    private int numberOfTanks=NewGame.getNumOfTanks();
     private int turnNumber=1;
+    private long startTime;
     private Tank[] tank;
     private Tank currentTank;
+
     Thread[] tankThreads;
     private Player player1 = new Player(NewGame.getColor1(),NewGame.getName1());
     private Player player2 = new Player(NewGame.getColor2(),NewGame.getName2());
-
+    public Color firstColor=NewGame.getRealColor1(), secondColor=NewGame.getRealColor2();
+    private Font font1=new Font("Calibri",Font.BOLD,20), font2=new Font("Calibri",Font.BOLD,12);
+    private int direction;
     /**
      * Konstruktor klasy <code>GamePanel</code> przyjmuje wartości szerokości i wysokości panelu rozgrywki.
      * @param x Szerokość panelu gry
@@ -34,6 +38,7 @@ public class GamePanel extends JPanel implements KeyListener {
         tank = new Tank[numberOfTanks];
         tankThreads = new Thread[numberOfTanks];
         createTanks(tank);
+        setDoubleBuffered(true);
         setFocusable(true);
 
     }
@@ -42,19 +47,43 @@ public class GamePanel extends JPanel implements KeyListener {
         for (int i=0;i<numberOfTanks;i++) {
             tankThreads[i]=new Thread(tank[i]);
             tank[i] = new Tank(width, 200, tankThreads[i]);
+            if(i%2==0)
+                tank[i].colorOfTank=firstColor;
+            else
+                tank[i].colorOfTank=secondColor;
         }
 
-        selectATank(tank[1]);
+        selectATank(tank[0]);
         return tank;
     }
 
     public void selectATank(Tank tank){
 
         currentTank=tank;
-        addKeyListener(this);
+        //addKeyListener(this);
         System.out.println("teraz czolg"+currentTank.getX());
         //currentTank.addNotify();
+        currentTank.setReadyToShot(true);
         repaint();
+    }
+    public void moveOfTank(int dir) throws InterruptedException {
+        currentTank.setCurrentTankStartPosition(currentTank.getX());
+        currentTank.setEndOfMove(false);
+        direction=dir;
+    }
+
+    public void shot(){
+        currentTank.setReadyToShot(true);
+        currentTank.releaseTheBullet();
+        repaint();
+    }
+
+    public void setAngle(int angle){
+        currentTank.setAngleOfShot(angle);
+    }
+
+    public void setStregth(int strength){
+        currentTank.setStrengthOfShot(strength);
     }
 
     public void nextTurn(){
@@ -85,27 +114,36 @@ public class GamePanel extends JPanel implements KeyListener {
         int[] formax = new int[2];
         loadMap(formax);
 
-        for(int i=0;i<getWidth();i++)
-        {
-            x=i;
-            x2=(i+1);
-            y =  200 + formax[0] * Math.sin((double)i/50) + formax[1];
-            y2 =  200 + formax[0] * Math.sin((double)(i+1)/50) + formax[1];
-            for(int j=0;j<numberOfTanks;j++) {
-                tank[j].setyCoordinates((int) y, i);
-            }
-            //y=200+((Math.sin(i/10))*100);
-            //y = 200 + Integer.parseInt(a) * Math.pow(i,2) + Integer.parseInt(b) + i;
-            //y2 = 200 + Integer.parseInt(a) * Math.pow(i+1,2) + Integer.parseInt(b) + (i+1);
-            //y2 = 200 + (Math.sin((i + 1) / 10) * 100));
-            g.setColor(Color.black);
-            // line[i]=new Line2D.Double(x,y,x2,y2);
-            g.drawLine((int)x,(int)y,(int)x2,(int)y2);
-        }
 
-        System.out.println(currentTank.getX()+"current Tank");
+        int[] groundCoordinates = new int[this.getWidth()+2];
+        groundCoordinates=countGroundCoordinates(formax);
+        Polygon ground = new Polygon();
+
+        ground.addPoint(0,this.getHeight());
+        for (int i = 1; i<this.getWidth()+1;i++){
+            ground.addPoint(i,groundCoordinates[i]);
+        }
+        ground.addPoint(this.getWidth()-1,this.getHeight());
+        ground.addPoint(0,this.getHeight());
+
+        for(int i=0;i<this.getWidth();i++)
+        {
+            for(int j=0;j<numberOfTanks;j++) {
+                tank[j].setyCoordinates(groundCoordinates[i+1], i);
+            }
+            // line[i]=new Line2D.Double(x,y,x2,y2);
+        }
+        // normowanie punktów
+        float xScale = (float) getWidth()/640;
+        float yScale = (float) getHeight()/380;
+        // skalowanie komponentów
+        ((Graphics2D) g).scale(xScale, yScale);
+        g.setColor(Color.green);
+        g.fillPolygon(ground);
+        g.drawPolygon(ground);
+        // System.out.println(currentTank.getX()+"current Tank");
         try {
-            currentTank.move();
+            currentTank.move(direction);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -114,13 +152,39 @@ public class GamePanel extends JPanel implements KeyListener {
             tank[j].setRandomY();
             tank[j].draw(g);
         }
+        if(turnNumber%2==1) {
+            g.setFont(font1);
+            g.setColor(firstColor);
+            g.drawString(player1.getName(), getWidth() / 20, getHeight() / 20);
+            g.drawString(Integer.toString(player1.getPoints()), getWidth() / 20, getHeight() / 20 + 20);
+            g.setFont(font2);
+            g.setColor(secondColor);
+            g.drawString(player2.getName(), 5 * getWidth() / 8, getHeight() / 20);
+            g.drawString(Integer.toString(player2.getPoints()), 5 * getWidth() / 8, getHeight() / 20 + 20);
+        }
+        else {
+            g.setFont(font2);
+            g.setColor(firstColor);
+            g.drawString(player1.getName(), getWidth() / 20, getHeight() / 20);
+            g.drawString(Integer.toString(player1.getPoints()), getWidth() / 20, getHeight() / 20 + 20);
+            g.setFont(font1);
+            g.setColor(secondColor);
+            g.drawString(player2.getName(), 5 * getWidth() / 8, getHeight() / 20);
+            g.drawString(Integer.toString(player2.getPoints()), 5 * getWidth() / 8, getHeight() / 20 + 20);
+        }
 
-        g.drawString(player1.getName(),getWidth()/20,getHeight()/20);
-        g.drawString(player2.getName(),7*getWidth()/8,getHeight()/20);
-        g.drawString(Integer.toString(player1.getPoints()),getWidth()/20,getHeight()/20 + 20);
-        g.drawString(Integer.toString(player2.getPoints()),7*getWidth()/8,getHeight()/20 + 20);
+
 
         repaint();
+    }
+
+    public int[] countGroundCoordinates(int[] coefficient){
+        int[] groundCoordinates = new int[this.getWidth()+2];
+
+        for (int i = 1; i<this.getWidth()+2;i++) {
+            groundCoordinates[i] =(int) (200 + coefficient[0] * Math.sin((double) (i) / 50) + coefficient[1]);
+        }
+        return groundCoordinates;
     }
 
 
@@ -135,6 +199,7 @@ public class GamePanel extends JPanel implements KeyListener {
      * @return Współczynniki funkcji
      * @throws IOException
      */
+
 
     private int[] loadMap(int[] form)
     {
@@ -155,6 +220,8 @@ public class GamePanel extends JPanel implements KeyListener {
         return form;
     }
 
+
+
     @Override
     public void keyTyped(KeyEvent e) {
     }
@@ -174,6 +241,7 @@ public class GamePanel extends JPanel implements KeyListener {
             currentTank.setXDirection(1);
         }
         if(keyCode == e.VK_SPACE){
+            //currentTank.startTime=System.currentTimeMillis();
             currentTank.releaseTheBullet();
         }
     }
